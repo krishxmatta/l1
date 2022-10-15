9eb4f0e74075f5914372e5120b95cf1858f9bea3
Krish Matta <self@krishxmatta.dev>
2022-10-15 15:48:59 -0400

    Implement set equality

--- src/interpreter/pure_set.cpp
+++ src/interpreter/pure_set.cpp
@@ -1,2 +1,28 @@
 #include "pure_set.h"
 using namespace std;
+
+bool pure_set_equals(pure_set a, pure_set b) {
+  if(a.children.size() != b.children.size()) {
+    return false;
+  }
+
+  int sz = a.children.size();
+
+  vector<bool> vis(sz, false);
+
+  for(int i = 0; i < sz; i++) {
+    bool found = false;
+    for(int j = 0; j < sz && !found; j++) {
+      if(!vis[j] && pure_set_equals(a.children[i], b.children[j])) {
+        found = true;
+        vis[j] = true;
+      }
+    }
+
+    if(!found) {
+      return false;
+    }
+  }
+  
+  return true;
+}
--- src/interpreter/pure_set.h
+++ src/interpreter/pure_set.h
@@ -3,3 +3,5 @@
 struct pure_set {
   std::vector<pure_set> children;
 };
+
+bool pure_set_equals(pure_set a, pure_set b);
